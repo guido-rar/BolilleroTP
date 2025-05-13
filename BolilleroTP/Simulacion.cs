440f9d57cb55bdc29ddf92f8f389b0d55278508c
Walter-Cooking <walterbenitez00@gmail.com>
2025-05-13 11:49:06 -0300

    Simulación asincrónica
    Co-Authored-By: guido-rar <177235382+guido-rar@users.noreply.github.com>

--- BolilleroTP/Simulacion.cs
+++ BolilleroTP/Simulacion.cs
@@ -3,43 +3,63 @@
     public class Simulacion
     {
 
-            public long SimulacionSinHilos(Bolillero bolilla1, int cantSimu)
+        public long SimulacionSinHilos(Bolillero bolilla1, int cantSimu)
+        {
+
+            long cantWin = 0;
+            for (int i = 0; i < cantSimu; i++)
             {
-
-                long cantWin = 0;
-                for (int i = 0; i < cantSimu; i++)
-                {
-                    cantWin += bolilla1.JugarNVeces(cantSimu, false);
-                }
-
-                return cantWin;
+                cantWin += bolilla1.JugarNVeces(cantSimu, false);
             }
 
-            public long SimularConHilos(Bolillero bolillero, int cantSimu, int cantHilos)
-            {
-                Task<long>[] tareas = new Task<long>[cantHilos];
-
-
-                int simus = cantSimu / cantHilos;
-                int simusEx = cantSimu % cantHilos;
-                int simusPorHilo = 0;
-                for (int i = 0; i < cantHilos; i++)
-                {
-                    Bolillero clon = bolillero.Clonar();
-                    simusPorHilo = simus + (i < simusEx ? 1 : 0);
-
-                    tareas[i] = (Task<long>.Run(() =>
-
-                        clon.JugarNVeces(simusPorHilo, false)
-                    ));
-                }
-
-                Task.WaitAll(tareas);
-
-                long totalWins = tareas.Sum(t => t.Result);
-                return totalWins;
-            }
+            return cantWin;
         }
+
+        public long SimularConHilos(Bolillero bolillero, int cantSimu, int cantHilos)
+        {
+
+            Task.WaitAll(OptiSimu(bolillero, cantSimu, cantHilos));
+
+            long totalWins = OptiSimu(bolillero, cantSimu, cantHilos).Sum(t => t.Result);
+            return totalWins;
+        }
+
+        public async Task<long> SimularConHilosAsync(Bolillero bolillero, int cantSimu, int cantHilos)
+        {
+
+            await Task.WhenAll(OptiSimu(bolillero, cantSimu, cantHilos));
+
+
+            long totalWins = OptiSimu(bolillero, cantSimu, cantHilos).Sum(t => t.Result);
+            return totalWins;
+        }
+
+
+        public Task<long>[] OptiSimu(Bolillero bolillero, int cantSimu, int cantHilos)
+        {
+            Task<long>[] tareas = new Task<long>[cantHilos];
+         
+            int simus = cantSimu / cantHilos;
+            int simusEx = cantSimu % cantHilos;
+            int simusPorHilo = 0;
+            for (int i = 0; i < cantHilos; i++)
+            {
+                Bolillero clon = bolillero.Clonar();
+                simusPorHilo = simus + (i < simusEx ? 1 : 0);
+
+                tareas[i] = (Task<long>.Run(() =>
+
+                    clon.JugarNVeces(simusPorHilo, false)
+                ));
+            }
+            return tareas;
+        }
+
+
     }
+}
+
+
+    
 
 
--- TestBolillero/SimulacionTest.cs
+++ TestBolillero/SimulacionTest.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Xunit;
+using BolilleroTP;
 
-namespace TestBolillero
+namespace TestSimulaciones
 {
-    internal class SimulacionTest
+    public class SimulacionTest
     {
+        private Bolillero bolillero;
+        private Simulacion simulacion = new Simulacion();
+
+        public SimulacionTest()
+        {
+            List<int> numerosWin = new List<int> { 0, 1, 2 };
+            bolillero = new Bolillero(numerosWin, 1, 3);
+        }
+
+        [Fact]
+        public void SimuHilosOK()
+        {
+            bool SiempreGana = false;
+            long result = simulacion.SimularConHilos(bolillero, 100000000, 5);
+            if (result > 0)
+            {
+                SiempreGana = true;
+            }
+
+            Assert.True(SiempreGana);
+        }
     }
 }
